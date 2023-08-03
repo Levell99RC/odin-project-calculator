4c3d7ae9c108bbb9e0aa311c9184d745cf6b70b1
Levell99RC <vincentsong96@gmail.com>
2023-08-02 23:59:44 -0400

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let number1, number2, operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +14,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*': 
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
 }
