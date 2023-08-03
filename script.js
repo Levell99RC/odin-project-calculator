c6a65183e280699ed9bddfaf5927c5a9c1f534ac
Levell99RC <vincentsong96@gmail.com>
2023-08-03 11:20:44 -0400

    Add functionality for number buttons and display

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let number1, number2, operator;
+let number1 = ''; 
+let number2 = '';
+let operator = '';
+
+const calc = document.querySelector('.calculator');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+display.textContent = ''; //Initialize display
+
+buttons.forEach((item) => {
+    item.addEventListener('click', () => {
+        if(item.textContent >= '0' && item.textContent <= '9') {
+            number1 += item.textContent;
+            display.textContent = number1;
+        } else if (item.textContent === 'C') {
+            display.textContent = '';
+            number1 = '';
+            number2 = '';
+            operator = '';
+        } else if (item.textContent === '=') {
+            
+        } else { 
+            //Else is any regular operator
+
+        }
+    });
+})
 
 function add(a, b) {
     return a + b;
@@ -25,10 +52,10 @@ function operate(operator, num1, num2) {
             return subtract(num1, num2);
             break;
         case '*': 
-            multiply(num1, num2);
+            return multiply(num1, num2);
             break;
         case '/':
-            divide(num1, num2);
+            return divide(num1, num2);
             break;
     }
 }
